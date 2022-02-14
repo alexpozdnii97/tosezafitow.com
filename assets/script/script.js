d2ed48cc3e1ae505c7cbbfda25db700a0642b015
Baroslav <75853671+Baroslav@users.noreply.github.com>
2022-02-14 13:59:13 +0200

    Bogdan
    Added Slider on Blog Post page

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
         $('.header_burger,.navBar_menu').toggleClass('active')
     })
 })
+
+
+if (window.matchMedia("(max-width: 992px)").matches) {
+    $('.subBlogPostSectionMobile').slick({
+     dots: false,
+     infinite: true,
+     speed: 300,
+     arrows: true,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+ });
+} 
